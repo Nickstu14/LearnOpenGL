92e65c4d4c1e33a1788d04a9f99ca21d599c40bc
nickstu14 <nickstu14@hotmail.co.uk>
2018-03-17 22:09:04 +0000

    Started the maths

--- OpenGL/OpenGL/src/Texture.cpp
+++ OpenGL/OpenGL/src/Texture.cpp
@@ -1,5 +1,5 @@
 #include "Texture.h"
-#include "Vender\stb_Image\stb_image.h"
+#include "stb_Image\stb_image.h"
 
 Texture::Texture(const std::string & path)
 	: m_RenderID(0), m_FilePath(path), m_LocalBuffer(nullptr), 
